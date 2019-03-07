882b24242298ec84177bdd1e7b2fd02c98b45299
1wmcmillin <42749672+1wmcmillin@users.noreply.github.com>
2019-03-07 13:36:23 -0600

    Add files via upload

---
+++ toggleFunctions.js
@@ -0,0 +1,38 @@
+function green25(){
+
+
+}
+
+
+function green50(){
+
+
+}
+
+
+function green75(){
+
+
+}
+
+
+function green100(){
+
+
+}
+
+
+function smallToggle(){
+
+
+}
+
+
+function mediumToggle(){
+
+}
+
+
+function largeToggle(){
+
+}
